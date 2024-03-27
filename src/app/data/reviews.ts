e7a86815236ccf4a3df1054e23177dbb6bd5caf0
FriedRubyjane <wangjinhyun@gmail.com>
2024-03-27 19:05:33 +0300

    add reviews

--- src/app/data/reviews.ts
+++ src/app/data/reviews.ts
@@ -1,14 +1,14 @@
 export const reviews = [
   {
     id: 1,
-    avatar: '../../../public/icons/avatars/reviews/irina.png',
+    avatar: '/icons/avatars/reviews/irina.png',
     name: 'Леонова Ирина',
     title: 'Title',
     review: 'Review',
   },
   {
     id: 2,
-    avatar: '../../../public/icons/avatars/reviews/pavel.png',
+    avatar: './icons/avatars/reviews/pavel.png',
     name: 'Трюфилькин Павел',
     title: 'Title',
     review: 'Review',
